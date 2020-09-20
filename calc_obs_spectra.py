92af765a622c7709ad5f8e3faf9ce18e8f632f96
Tara Nye <tara.nye96@gmail.com>
2020-09-20 11:43:27 -0700

    added files

---
+++ calc_obs_spectra.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 19:31:02 2020
+
+@author: tnye
+"""
+
+###############################################################################
+# Script that calculates the Fourier spectra for the observed Mentawai
+# waveforms and saves them as .csv files.  These files are used in 
+# synthetic_calc_mpi.py to make spectra comparison plots. 
+###############################################################################
+
+
+# Imports
+from glob import glob
+import numpy as np
+import pandas as pd
+from obspy import read
+import IM_fns
+
+data_types = ['disp', 'acc', 'vel']
+
+for data in data_types:
+    ### Set paths and parameters #### 
+
+    # Data directory                          
+    data_dir = '/Users/tnye/tsuquakes/data'
+    
+    # Table of earthquake data
+    eq_table_path = '/Users/tnye/tsuquakes/data/misc/events.csv'   
+    eq_table = pd.read_csv(eq_table_path)
+    
+    ### Get event data ###
+    eventname = 'Mentawai2010'
+    country = eq_table['Country'][11]
+    hyplon = eq_table['Longitude'][11]
+    hyplat = eq_table['Latitude'][11]
+    hypdepth = eq_table['Depth (km)'][11]
+    
+    # Filtering
+    threshold = 0.0
+    fcorner = 1/15.                          # Frequency at which to high pass filter
+    order = 2                                # Number of poles for filter  
+            
+    ##################### Data Processing and Calculations ####################
+    
+    # Obs data directories
+    disp_dir = '/Users/tnye/tsuquakes/data/Mentawai2010/disp_corr'
+    acc_dir = '/Users/tnye/tsuquakes/data/Mentawai2010/accel_corr'
+    vel_dir = '/Users/tnye/tsuquakes/data/Mentawai2010/vel_corr'
+            
+    if data == 'disp':
+        metadata_file = data_dir + '/' + eventname + '/' + eventname + '_disp.chan'
+        obs_files = np.array(sorted((glob(disp_dir + '/*'))))
+        filtering = False
+        dtype = 'disp'
+    elif data == 'acc':
+        metadata_file = data_dir + '/' + eventname + '/' + eventname + '_sm.chan'
+        obs_files = np.array(sorted((glob(acc_dir + '/*'))))
+        filtering = True
+        dtype = 'sm'
+    elif data == 'vel':
+        metadata_file = data_dir + '/' + eventname + '/' + eventname + '_sm.chan'
+        obs_files = np.array(sorted((glob(vel_dir + '/*'))))
+        filtering = True
+        dtype = 'sm'
+    
+    metadata = pd.read_csv(metadata_file, sep='\t', header=0,
+                          names=['net', 'sta', 'loc', 'chan', 'lat',
+                                 'lon', 'elev', 'samplerate', 'gain', 'units'])
+    
+    # There might be white spaces in the station name, so remove those
+    metadata.sta = metadata.sta.astype(str)
+    metadata.sta = metadata.sta.str.replace(' ','')
+    
+    syn_freqs = []
+    syn_amps = []
+    obs_freqs = []
+    obs_amps = []
+    hypdists = []
+    
+    
+    ################################### Observed ###########3######################
+    
+    # Create lists to add station names, channels, and miniseed files to 
+    stn_name_list = []
+    channel_list = []
+    mseed_list = []
+    
+    # Group all files by station
+    N = 3
+    stn_files = [obs_files[n:n+N] for n in range(0, len(obs_files), N)]
+    
+    # Loop over files to get the list of station names, channels, and mseed files 
+    for station in stn_files:
+        components = []
+        mseeds = []
+    
+        stn_name = station[0].split('.')[0].split('/')[-1]
+        if stn_name != 'SISI':
+            stn_name_list.append(stn_name)
+            
+            for mseed_file in station:
+                channel_code = mseed_file.split('/')[-1].split('.')[1]
+                components.append(channel_code)
+                mseeds.append(mseed_file)
+        
+            channel_list.append(components)
+            mseed_list.append(mseeds)
+    
+    # Loop over the stations for this earthquake, and start to run the computations:
+    for i, station in enumerate(stn_name_list):
+            
+        # Get the components for this station (E, N, and Z):
+        components = []
+        for channel in channel_list[i]:
+            components.append(channel[2])
+            
+        # Get the metadata for this station from the chan file - put it into
+        #     a new dataframe and reset the index so it starts at 0
+        if country == 'Japan':
+            station_metadata = metadata[(metadata.net == station[0:2]) & (metadata.sta == station[2:])].reset_index(drop=True)
+            
+        else:
+            station_metadata = metadata[metadata.sta == station].reset_index(drop=True)       # what is going on here
+    
+                    
+        # Pull out the data. Take the first row of the subset dataframe, 
+        #    assuming that the gain, etc. is always the same:
+        stnetwork = station_metadata.loc[0].net
+        stlon = station_metadata.loc[0].lon
+        stlat = station_metadata.loc[0].lat
+        stelev = station_metadata.loc[0].elev
+        stsamprate = station_metadata.loc[0].samplerate
+        stgain = station_metadata.loc[0].gain
+    
+    
+        ######################### Start computations ##########################       
+        
+        # List for all spectra at station
+        station_spec = []
+    
+        # Get the components
+        components = np.asarray(components)
+            
+        # Get index for E component 
+        E_index = np.where(components=='E')[0][0]
+        # Read file into a stream object
+        E_record = read(mseed_list[i][E_index])
+    
+    
+        ####################### IMs ########################
+    
+        # Calc Spectra
+        E_spec_data, freqE, ampE = IM_fns.calc_spectra(E_record, dtype)
+        
+        # Append spectra to observed list
+        obs_freqs.append(freqE.tolist())
+        obs_amps.append(ampE.tolist())
+        
+    freq_df = pd.DataFrame(obs_freqs)
+    amp_df = pd.DataFrame(obs_amps)
+    flatfile_df = pd.concat([freq_df, amp_df], axis=1)
+    
+    flatfile_df.to_csv(f'/Users/tnye/tsuquakes/data/obs_spectra/{data}_spec.csv',index=False)
+    
---
+++ comparison_fns.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 19:52:17 2020
+
+@author: tnye
+"""
+
+###############################################################################
+# Module with functions used to make comparison plots between observed and 
+# synthetic data. 
+###############################################################################
+
+
+def plot_spec_comp(syn_freqs, syn_amps, obs_freqs, obs_amps, stn_list, hypdists, data_type, home, parameter, project, run):
+    """
+    Makes a figure comparing observed spectra to synthetic spectra with
+    subplots for each station. 
+
+    Inputs:
+        syn_freqs(list): Array of list of frequencies obtained when computing
+                         Fourier spectra of the synthetics for each station
+        syn_amps(list): Array of list of amplitudes obtained when computing
+                        Fourier spectra of the synthetics for each station
+        obs_freqs(list): Array of list of frequencies obtained when computing
+                         Fourier spectra of the observed data for each station
+        obs_amps(list): Array of list of amplitudes obtained when computing
+        stn_list(list): List of station names
+                        Fourier spectra of the observed data for each station
+        hypdists(list): List of hypocentral distances correlating with the 
+                        stations used to get spectra
+        data_type(str): Type of data
+                            Options:
+                                disp
+                                acc
+                                vel
+        home(str): Base of path to save plots.
+        parameter(str): Name of parameter folder.
+        project(str): Name of simulation project.
+        run(str): Synthetics run number.
+
+    Output:
+            Just saves the plots to ther respective directories. 
+            
+    """
+    
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from os import path, makedirs
+            
+    # Set figure axes
+    if data_type == 'disp':
+       units = 'm*s'
+       ylim = 10**-4, 7*10**-1
+       xlim = 2*10**-3, 5*10**-1
+       dim = 5,3
+       figsize = 10,20
+    elif data_type == 'acc':
+       units = 'm/s'
+       ylim = 7*10**-15, 6*10**-1
+       xlim = .002, 10
+       dim = 6,3
+       figsize = 10,30
+    elif data_type == 'vel':
+       units = 'm'
+       ylim = 6*10**-15, 8*10**-2
+       xlim = .002, 10
+       dim = 6,3
+       figsize = 10,30
+    
+    # Sort hypdist and get indices
+    sort_id = np.argsort(hypdists)
+    sort_hypdists = np.sort(hypdists)
+    
+    # Sort freq and amps based off hypdist
+    def sort_list(list1, list2): 
+        zipped_pairs = zip(list2, list1) 
+        z = [x for _, x in sorted(zipped_pairs)] 
+        return z
+    
+    sort_syn_freqs = sort_list(syn_freqs, sort_id)
+    sort_syn_amps = sort_list(syn_amps, sort_id)
+    sort_obs_freqs = sort_list(obs_freqs, sort_id)
+    sort_obs_amps = sort_list(obs_amps, sort_id)
+    sort_stn_name = sort_list(stn_list, sort_id)
+    
+    if data_type == 'disp':
+        # Set up figure
+        fig, axs = plt.subplots(dim[0],dim[1],figsize=(10,12))
+        k = 0
+        for i in range(dim[0]):
+            for j in range(dim[1]):
+                if k+1 <= len(stn_list):
+                    axs[i][j].loglog(sort_syn_freqs[k],sort_syn_amps[k],lw=1,ls='--',label='synthetic')
+                    axs[i][j].loglog(sort_obs_freqs[k],sort_obs_amps[k],lw=1,ls='-',label='observed')
+                    axs[i][j].grid(linestyle='--')
+                    axs[i][j].set_xlim(xlim)
+                    axs[i][j].set_ylim(ylim)
+                    axs[i][j].set_title(sort_stn_name[k],fontsize=10)
+                    axs[i][j].text(0.65,5E-2,f'Hypdist={int(sort_hypdists[k])}km',
+                                   transform=axs[i][j].transAxes,size=7)
+                    if i < dim[0]-2:
+                        # plt.setp(axs[i][j], xticks=[])
+                        axs[i][j].set_xticklabels([])
+                    if i == dim[0]-2 and j == 0:
+                        # plt.setp(axs[i][j], xticks=[])
+                        axs[i][j].set_xticklabels([])
+                    if j > 0:
+                        # plt.setp(axs[i][j], yticks=[])
+                        axs[i][j].set_yticklabels([])
+                    k += 1
+        fig.text(0.51, 0.005, 'Frequency (Hz)', ha='center')
+        fig.text(0.005, 0.5, f'Amp ({units})', va='center', rotation='vertical')
+        handles, labels = axs[0][0].get_legend_handles_labels()
+        fig.legend(handles, labels, loc=(0.8,0.075), framealpha=None)
+        if data_type == 'disp':
+                fig.delaxes(axs[4][1])
+                fig.delaxes(axs[4][2])
+        else:
+                fig.delaxes(axs[5][1])
+                fig.delaxes(axs[5][2])
+        fig.suptitle('Fourier Spectra Comparison', fontsize=12, y=1)
+        fig.text(0.45, 0.125, (r"$\bf{" + 'Project:' + "}$" + '' + project))
+        fig.text(0.45, 0.1, (r'$\bf{' + 'Run:' + '}$' + '' + run))
+        fig.text(0.45, 0.075, (r'$\bf{' + 'DataType:' '}$' + '' + data_type))
+        plt.tight_layout()
+        
+        if not path.exists(f'{home}/{parameter}/{project}/plots/comparison/spectra'):
+            makedirs(f'{home}/{parameter}/{project}/plots/comparison/spectra')
+            
+        plt.savefig(f'{home}/{parameter}/{project}/plots/comparison/spectra/{run}_{data_type}.png', dpi=300)
+        plt.close()
+    
+    else:
+        # Set up figure
+        fig, axs = plt.subplots(dim[0],dim[1],figsize=figsize)
+        k = 0
+        for i in range(dim[0]):
+            for j in range(dim[1]):
+                if k+1 <= len(stn_list):
+                    axs[i][j].loglog(sort_syn_freqs[k],sort_syn_amps[k],lw=1,ls='--',label='synthetic')
+                    axs[i][j].loglog(sort_obs_freqs[k],sort_obs_amps[k],lw=1,ls='-',label='observed')
+                    axs[i][j].grid(linestyle='--')
+                    axs[i][j].text(0.7,5E-13,f'Hypdist={int(sort_hypdists[k])}km',size=7)
+                    axs[i][j].set_xlim(xlim)
+                    axs[i][j].set_ylim(ylim)
+                    axs[i][j].set_title(sort_stn_name[k],fontsize=10)
+                    if i < dim[0]-2:
+                        # plt.setp(axs[i][j], xticks=[])
+                        axs[i][j].set_xticklabels([])
+                    if i == dim[0]-2 and j == 0:
+                        # plt.setp(axs[i][j], xticks=[])
+                        axs[i][j].set_xticklabels([])
+                    if j > 0:
+                        # plt.setp(axs[i][j], yticks=[])
+                        axs[i][j].set_yticklabels([])
+                    k += 1
+        fig.text(0.51, 0.005, 'Frequency (Hz)', ha='center')
+        fig.text(0.005, 0.5, f'Amp ({units})', va='center', rotation='vertical')
+        handles, labels = axs[0][0].get_legend_handles_labels()
+        fig.legend(handles, labels, loc=(0.8,0.06), framealpha=None)
+        if data_type == 'disp':
+                fig.delaxes(axs[4][1])
+                fig.delaxes(axs[4][2])
+        else:
+                fig.delaxes(axs[5][1])
+                fig.delaxes(axs[5][2])
+        fig.suptitle('Fourier Spectra Comparison', fontsize=12, y=1)
+        fig.text(0.45, 0.115, (r"$\bf{" + 'Project:' + "}$" + '' + project))
+        fig.text(0.45, 0.09, (r'$\bf{' + 'Run:' + '}$' + '' + run))
+        fig.text(0.45, 0.065, (r'$\bf{' + 'DataType:' '}$' + '' + data_type))
+        plt.tight_layout()
+        
+        if not path.exists(f'{home}/{parameter}/{project}/plots/comparison/spectra'):
+            makedirs(f'{home}/{parameter}/{project}/plots/comparison/spectra')
+        
+        plt.savefig(f'{home}/{parameter}/{project}/plots/comparison/spectra/{run}_{data_type}.png', dpi=300)
+        plt.close()
+        
+        
+def plot_wf_comp(syn_times, syn_amps, obs_times, obs_amps, stn_list, hypdists, data_type, home, parameter, project, run):
+    """
+    Makes a figure comparing observed spectra to synthetic spectra with
+    subplots for each station. 
+
+    Inputs:
+        syn_freqs(list): Array of list of times for synthetic waveforms
+        syn_amps(list): Array of list of amplitudes for synthetic waveforms
+        obs_freqs(list): Array of list of times for observed waveforms
+        obs_amps(list): Array of list of amplitudes for observed waveforms
+        stn_list(list): List of station names
+                        Fourier spectra of the observed data for each station
+        hypdists(list): List of hypocentral distances correlating with the 
+                        stations used to get spectra
+        data_type(str): Type of data
+                            Options:
+                                disp
+                                acc
+                                vel
+        home(str): Base of path to save plots.
+        parameter(str): Name of parameter folder.
+        project(str): Name of simulation project.
+        run(str): Synthetics run number.
+
+    Output:
+            Just saves the plots to ther respective directories. 
+            
+    """
+    
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.dates as mdates
+    from os import path, makedirs
+      
+    # Set figure parameters based on data type
+    if data_type == 'disp':
+           units = 'm'
+           dim = 5,3
+           figsize = 10,20
+    elif data_type == 'acc':
+           units = 'm/s/s'
+           dim = 6,3
+           figsize = 10,30
+    elif data_type == 'vel':
+           units = 'm/s'
+           dim = 6,3
+           figsize = 10,30
+    
+    # Sort hypdist and get sorted indices
+    sort_id = np.argsort(hypdists)
+    sort_hypdists = np.sort(hypdists)
+    
+    # Function to sort list based on list of indices 
+    def sort_list(list1, list2): 
+        zipped_pairs = zip(list2, list1) 
+        z = [x for _, x in sorted(zipped_pairs)] 
+        return z 
+    
+    # Sort times and amps based off hypdist
+    sort_syn_times = sort_list(syn_times, sort_id)
+    sort_syn_amps = sort_list(syn_amps, sort_id)
+    sort_obs_times = sort_list(obs_times, sort_id)
+    sort_obs_amps = sort_list(obs_amps, sort_id)
+    sort_stn_name = sort_list(stn_list, sort_id)
+    
+    # Make figure and subplots
+    if data_type == 'disp':
+        # Set up figure
+        fig, axs = plt.subplots(dim[0],dim[1],figsize=figsize)
+        k = 0     # subplot index
+        # Loop rhough rows
+        for i in range(dim[0]):
+            # Loop through columns
+            for j in range(dim[1]):
+                # Only make enough subplots for length of station list
+                if k+1 <= len(stn_list):
+                    axs[i][j].plot(sort_syn_times[k],sort_syn_amps[k],
+                                     color='C1',lw=0.4,label='synthetic')
+                    axs[i][j].plot(sort_obs_times[k],sort_obs_amps[k],
+                                     'k-',lw=0.4,label='observed')
+                    axs[i][j].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+                    axs[i][j].set_title(sort_stn_name[k],fontsize=10)
+                    axs[i][j].text(0.625,5E-2,f'Hypdist={int(sort_hypdists[k])}km',
+                                    transform=axs[i][j].transAxes,size=7)
+                    axs[i][j].text(0.025,5E-2,'LXE',transform=axs[i][j].transAxes,size=7)
+                    if i < dim[0]-2:
+                        axs[i][j].set_xticklabels([])
+                    if i == dim[0]-2 and j == 0:
+                        axs[i][j].set_xticklabels([])
+                    k += 1
+        fig.text(0.5, 0.005, 'UTC Time(hr:min:sec)', ha='center')
+        fig.text(0.005, 0.5, f'Amplitude ({units})', va='center', rotation='vertical')
+        handles, labels = axs[0][0].get_legend_handles_labels()
+        fig.legend(handles, labels, loc=(0.74,0.09), framealpha=None)
+        if data_type == 'disp':
+                fig.delaxes(axs[4][1])
+                fig.delaxes(axs[4][2])
+        else:
+                fig.delaxes(axs[5][1])
+                fig.delaxes(axs[5][2])
+        fig.suptitle('Waveform Comparison', fontsize=12, y=1)
+        fig.text(0.385, 0.135, (r"$\bf{" + 'Project:' + "}$" + '' + project))
+        fig.text(0.385, 0.115, (r'$\bf{' + 'Run:' + '}$' + '' + run))
+        fig.text(0.385, 0.09, (r'$\bf{' + 'DataType:' '}$' + '' + data_type))
+        plt.subplots_adjust(left=0.1, right=0.9, bottom=0.075, top=0.925,
+                            wspace=0.3, hspace=0.4)
+    
+        if not path.exists(f'{home}/{parameter}/{project}/plots/comparison/wf/'):
+          makedirs(f'{home}/{parameter}/{project}/plots/comparison/wf')
+      
+        plt.savefig(f'{home}/{parameter}/{project}/plots/comparison/wf/{run}_{data_type}.png', dpi=300)
+        plt.close()
+        
+        
+    else:
+        # Set up figure
+        fig, axs = plt.subplots(dim[0],dim[1],figsize=(10,15))
+        # fig, axs = plt.subplots(dim[0],dim[1])
+        k = 0     # subplot index
+        # Loop through rows
+        for i in range(dim[0]):
+            # Loop through columns 
+            for j in range(dim[1]):
+                # Only make enough subplots for length of station list
+                if k+1 <= len(stn_list):
+                    axs[i][j].plot(sort_syn_times[k],sort_syn_amps[k],
+                                     color='C1',lw=0.4,label='synthetic')
+                    axs[i][j].plot(sort_obs_times[k],sort_obs_amps[k],
+                                     'k-',lw=0.4,label='observed')
+                    axs[i][j].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+                    axs[i][j].text(0.7,5E-13,f'Hypdist={int(sort_hypdists[k])}km',size=7)
+                    axs[i][j].text(0.025,5E-2,'HNE',transform=axs[i][j].transAxes,size=7)
+                    axs[i][j].set_title(sort_stn_name[k],fontsize=10)
+                    if i < dim[0]-2:
+                        axs[i][j].set_xticklabels([])
+                    if i == dim[0]-2 and j == 0:
+                        axs[i][j].set_xticklabels([])
+                    k += 1
+        fig.text(0.51, 0.005, 'UTC Time(hr:min:sec))', ha='center')
+        fig.text(0.005, 0.5, f'Amplitude ({units})', va='center', rotation='vertical')
+        handles, labels = axs[0][0].get_legend_handles_labels()
+        fig.legend(handles, labels, loc=(0.74,0.08), framealpha=None)
+        if data_type == 'disp':
+                fig.delaxes(axs[4][1])
+                fig.delaxes(axs[4][2])
+        else:
+                fig.delaxes(axs[5][1])
+                fig.delaxes(axs[5][2])
+        fig.suptitle('Waveform Comparison', fontsize=12, y=1)
+        fig.text(0.435, 0.125, (r"$\bf{" + 'Project:' + "}$" + '' + project))
+        fig.text(0.435, 0.105, (r'$\bf{' + 'Run:' + '}$' + '' + run))
+        fig.text(0.435, 0.08, (r'$\bf{' + 'DataType:' '}$' + '' + data_type))
+        # fig.tight_layout()
+        plt.subplots_adjust(left=0.1, right=0.9, bottom=0.075, top=0.925,
+                            wspace=0.4, hspace=0.4)
+    
+        if not path.exists(f'{home}/{parameter}/{project}/plots/comparison/wf/'):
+            makedirs(f'{home}/{parameter}/{project}/plots/comparison/wf')
+        
+        plt.savefig(f'{home}/{parameter}/{project}/plots/comparison/wf/{run}_{data_type}.png', dpi=300)
+        plt.close()
+        
